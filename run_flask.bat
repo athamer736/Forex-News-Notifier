2c2d82746f0cdfe03d95abc37e9db3f33c3820d4
Ahmed Thamer <athamer736@gmail.com>
2025-02-12 12:25:21 +0000

    Update .gitignore and remove batch files from tracking

--- run_flask.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-title Forex News Notifier - Flask Server 
-color 0B 
-set PYTHONPATH=C:\Projects\forex_news_notifier 
-call venv\Scripts\activate 
-python app.py 
-pause 
--- run_frontend.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-title Forex News Notifier - Frontend 
-color 0D 
-cd frontend 
-npm run dev 
-pause 
