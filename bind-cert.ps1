e82ebef15c7cbfde452005ff486cefe83d704b29
Ahmed Thamer <athamer736@gmail.com>
2025-02-16 19:22:23 +0000

    Binding SSL Certificate

--- bind-cert.ps1
+++ bind-cert.ps1
@@ -2,7 +2,7 @@
 Import-Module WebAdministration
 
 # Certificate thumbprint
-$thumbprint = "71F1F7C0984196C468404F281CE0FE617E5EFA3B"
+$thumbprint = "AA79E98B5F0900A7B04586CF87126EE5F8695F0B"
 $siteName = "ForexNewsNotifier"
 
 Write-Host "Removing existing bindings..."
@@ -10,34 +10,43 @@ Get-WebBinding -Name $siteName | Remove-WebBinding
 
 Write-Host "Adding new HTTP binding..."
 New-WebBinding -Name $siteName -Protocol "http" -Port 80 -HostHeader "fxalert.co.uk"
+New-WebBinding -Name $siteName -Protocol "http" -Port 80 -HostHeader "www.fxalert.co.uk"
 
-Write-Host "Adding new HTTPS binding..."
+Write-Host "Adding new HTTPS bindings..."
+New-WebBinding -Name $siteName -Protocol "https" -Port 443 -HostHeader "fxalert.co.uk" -SslFlags 1
+New-WebBinding -Name $siteName -Protocol "https" -Port 443 -HostHeader "www.fxalert.co.uk" -SslFlags 1
 New-WebBinding -Name $siteName -Protocol "https" -Port 3000 -HostHeader "fxalert.co.uk" -SslFlags 1
+New-WebBinding -Name $siteName -Protocol "https" -Port 5000 -HostHeader "fxalert.co.uk" -SslFlags 1
 
 Write-Host "Setting physical path..."
 Set-ItemProperty "IIS:\Sites\$siteName" -Name "physicalPath" -Value "C:\FlaskApps\forex_news_notifier\frontend"
 
-Write-Host "Configuring SSL certificate..."
+Write-Host "Configuring SSL certificates..."
 # Create a new GUID for the application
 $appid = [System.Guid]::NewGuid().ToString("B")
 
-# Remove any existing certificate bindings for port 3000
+# Remove any existing certificate bindings
+netsh http delete sslcert ipport=0.0.0.0:443 | Out-Null
 netsh http delete sslcert ipport=0.0.0.0:3000 | Out-Null
+netsh http delete sslcert ipport=0.0.0.0:5000 | Out-Null
 
-# Add the new certificate binding
-$result = netsh http add sslcert ipport=0.0.0.0:3000 certhash=$thumbprint appid=$appid
-
-if ($LASTEXITCODE -eq 0) {
-    Write-Host "SSL certificate binding successful!"
-} else {
-    Write-Host "SSL certificate binding failed. Trying alternative method..."
-    # Try alternative binding method
-    $result = netsh http add sslcert ipport=0.0.0.0:3000 certhash=$thumbprint appid=$appid certstorename=MY sslctlstorename=MY
+# Add the new certificate bindings
+$ports = @(443, 3000, 5000)
+foreach ($port in $ports) {
+    Write-Host "Binding certificate to port $port..."
+    $result = netsh http add sslcert ipport=0.0.0.0:$port certhash=$thumbprint appid=$appid
     
-    if ($LASTEXITCODE -eq 0) {
-        Write-Host "SSL certificate binding successful with alternative method!"
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "Standard binding failed for port $port, trying alternative method..."
+        $result = netsh http add sslcert ipport=0.0.0.0:$port certhash=$thumbprint appid=$appid certstorename=MY sslctlstorename=MY
+        
+        if ($LASTEXITCODE -eq 0) {
+            Write-Host "Successfully bound certificate to port $port using alternative method"
+        } else {
+            Write-Host "Failed to bind certificate to port $port: $result"
+        }
     } else {
-        Write-Host "SSL certificate binding failed with error: $result"
+        Write-Host "Successfully bound certificate to port $port"
     }
 }
 
@@ -45,6 +54,10 @@ Write-Host "Restarting IIS..."
 iisreset /restart
 
 Write-Host "Configuration complete. Verifying bindings..."
-Get-WebBinding -Name $siteName | Format-Table Protocol, BindingInformation
+Get-WebBinding -Name $siteName | Format-Table Protocol, bindingInformation
+
 Write-Host "`nVerifying SSL certificate bindings..."
-netsh http show sslcert ipport=0.0.0.0:3000 
+foreach ($port in $ports) {
+    Write-Host "`nPort $port bindings:"
+    netsh http show sslcert ipport=0.0.0.0:$port
+} 
